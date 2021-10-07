bce03c586cd67a0cc13751bf5a95bdf67c51279e
Edoardo <ercolino.edoardo@gmail.com>
2021-10-07 14:38:55 +0200

    Scaffolfing

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- 1) Creare una variabile con un paragrafo di testo a vostra scelta. -->
+<!-- 2) Stampare a schermo il paragrafo e la sua lunghezza. -->
+<!-- 3) Una parola da censurare viene passata dall'utente tramite parametro GET. -->
+<!-- 4) Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.  -->
+
+<?php 
+    $testo = '';
+?>
